08d479dca18a81a195fab55048e82721e4989167
Adrián Casas <adriancasasizquierdo@gmail.com>
2025-11-11 09:16:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8436151574-d1686",
+  "appId": "1:708514552595:web:a36e43cb63e3be931998e6",
+  "apiKey": "AIzaSyDpO90Geu2HIAwOkqjjSOh_UzLB48VV78g",
+  "authDomain": "studio-8436151574-d1686.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "708514552595"
+};
